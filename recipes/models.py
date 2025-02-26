c81d3f8e82fa2f947c935802442deb59636d0070
FMHagihara <fmhagihara@gmail.com>
2025-02-26 16:34:51 -0300

    Campos de receitas podem ser vazios

--- recipes/models.py
+++ recipes/models.py
@@ -22,9 +22,9 @@ class Recipe(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     is_published = models.BooleanField(default=False)
-    cover = models.ImageField(upload_to='recipes/covers/%Y/%m/%d/')
-    category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True)
-    author = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+    cover = models.ImageField(upload_to='recipes/covers/%Y/%m/%d/', blank=True, default='')
+    category = models.ForeignKey(Category, on_delete=models.SET_NULL, blank=True, null=True, default=None)
+    author = models.ForeignKey(User, on_delete=models.SET_NULL, blank=True, null=True, default=None)
 
     def __str__(self):
         return str(self.title)
--- recipes/views.py
+++ recipes/views.py
@@ -1,12 +1,31 @@
 from django.shortcuts import render
+from .models import Recipe
+from django.http import Http404
 
 from utils.recipes.factory import make_recipe
 
 
 # Create your views here.
 def home(request):
+    recipes = Recipe.objects.all().filter(
+        is_published=True
+        ).order_by('-id')
     return render(request, 'recipes/pages/home.html', context={
-        'recipes': [make_recipe() for _ in range(10)],
+        'recipes': recipes,
+    })
+
+def category(request, category_id):
+    recipes = Recipe.objects.filter(
+        category__id=category_id, 
+        is_published=True
+        ).order_by('-id')
+    
+    if not recipes:
+        raise Http404('Not found')
+    
+    return render(request, 'recipes/pages/category.html', context={
+        'recipes': recipes,
+        'title': f'{recipes.first().category.name} Category |'
     })
 
 def recipe(request, id):
