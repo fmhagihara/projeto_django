76b31ddc20a0102e8c4e9abafac4c02dee3da077
FMHagihara <fmhagihara@gmail.com>
2025-02-27 20:45:10 -0300

    Testes

--- recipes/models.py
+++ recipes/models.py
@@ -1,10 +1,11 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+
 # Create your models here.
 class Category(models.Model):
     name = models.CharField(max_length=65)
-    
+
     def __str__(self):
         return str(self.name)
 
@@ -22,9 +23,13 @@ class Recipe(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     is_published = models.BooleanField(default=False)
-    cover = models.ImageField(upload_to='recipes/covers/%Y/%m/%d/', blank=True, default='')
-    category = models.ForeignKey(Category, on_delete=models.SET_NULL, blank=True, null=True, default=None)
-    author = models.ForeignKey(User, on_delete=models.SET_NULL, blank=True, null=True, default=None)
+    cover = models.ImageField(
+        upload_to='recipes/covers/%Y/%m/%d/', blank=True, default='')
+    category = models.ForeignKey(
+        Category, on_delete=models.SET_NULL, blank=True, null=True,
+        default=None)
+    author = models.ForeignKey(
+        User, on_delete=models.SET_NULL, blank=True, null=True, default=None)
 
     def __str__(self):
         return str(self.title)
